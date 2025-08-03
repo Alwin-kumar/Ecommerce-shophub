dd5bef8bd3349878ea844c19bea5d192bffa1123
alwin.se2k6@gmail.com <edwinkumar12a207@gmail.com>
2025-08-03 18:33:50 +0530

    fix: corrected Newsletter import path in Shop.jsx

--- Frontend/src/Pages/Shop.jsx
+++ Frontend/src/Pages/Shop.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Hero from '../Components/Hero/Hero'
-import Popular from '../Components/Popular/Popular'
-import Offers from '../Components/Offers/Offers'
-import NewCollection from '../Components/NewCollection/NewCollection'
-
-import Newsletter from '../Components/Newletter/Newsletter'
-import Footer from '../Components/Footer/Footer'
+import React from 'react';
+import Hero from '../Components/Hero/Hero';
+import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
+import NewCollection from '../Components/NewCollection/NewCollection';
+import Newsletter from '../Components/Newsletter/Newsletter';  // ✅ fixed spelling
+import Footer from '../Components/Footer/Footer';
 
 const Shop = () => {
   return (
     <div>
-        <Hero/>
-        <Popular/>
-        <Offers/>
-        <NewCollection/>
-        <Newsletter/>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollection />
+      <Newsletter />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
